a8c45cae22be409daefd857119ab4044099f27e7
aacheng17 <aacheng17@gmail.com>
2025-01-28 01:33:24 -0800

    Just, show freqs, logView

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,37 @@ enum ScaleType {
 
 const allScaleTypes = [ ScaleType.EQUAL, ScaleType.JUST, ScaleType.PYTHAGOREAN ]
 
+const getGcd = (a:number,b:number): number => {
+  return b ? getGcd(b, a%b) : a;
+};
+
+const reduceFraction = (n:number, d:number) => {
+  const x = getGcd(n,d);
+  return [n/x, d/x];
+}
+const getId = (x:number[]) => `${x[0]},${x[1]}`
+const fromId = (x:string) => x.split(',').map(s => parseInt(s, 10))
+
+export class Freq {
+  n?: number
+  d?: number
+  ratio: number
+  
+  constructor(a: number, b?: number) {
+    if (b === undefined) {
+      this.ratio = a
+    } else {
+      this.n = a
+      this.d = b
+      this.ratio = this.n / this.d
+    }
+  }
+}
+
 function App() {
   const [notes, setNotes] = React.useState<number[]>([])
 
+  const [ logView, setLogView ] = React.useState(false)
   const [ referenceNote, setReferenceNote ] = React.useState(440)
   const octaveMult = 2
   const octavesBelow = 4
@@ -20,10 +48,14 @@ function App() {
   const [ scaleType, setScaleType ] = React.useState(ScaleType.EQUAL)
 
   // JUST
+  const [ powersOf2Only, setPowersOf2Only ] = React.useState(true)
+  const [ highestDenominator, setHighestDenominator ] = React.useState(16)
 
   // PYTHAGOREAN
   const [ baseNum, setBaseNum ] = React.useState(3)
   const [ baseDen, setBaseDen ] = React.useState(2)
+
+  const [ hoveredFreq, setHoveredFreq ] = React.useState<number>()
 
   const scaleTypeSelect = React.useMemo(() => {
     return (
@@ -61,20 +93,58 @@ function App() {
     setNotes(result)
   },[referenceNote])
 
+  const getEqualTemperamentRatios = React.useCallback(() => {
+    const k = Math.pow(octaveMult, 1/notesPerOctave)
+    const ratios = [k]
+    let ratio = k
+    for (let i=1; i<notesPerOctave-1; i++) {
+      ratio *= k
+      ratios.push(ratio)
+    }
+    return ratios
+  },[notesPerOctave])
+
+  const getJustIntonationRatios = React.useCallback(() => {
+    const possibleRatios = new Set<string>()
+    const ratios = []
+    let n = 2, d = 1;
+    while (d <= highestDenominator) {
+      const r = getId(reduceFraction(n, d))
+      if (!possibleRatios.has(r)) possibleRatios.add(r)
+      if (n / d >= 2) {
+        if (powersOf2Only) {
+          d*=2
+        } else { d+=1 }
+        n = d+1
+      } else {
+        n++
+      }
+    }
+    
+    for (const equalRatio of getEqualTemperamentRatios()) {
+      let lowestDiff = 1
+      let lowestId: string = ''
+      for (const x of possibleRatios) {
+        const diff = Math.abs(fromId(x)[0] / fromId(x)[1] - equalRatio)
+        if (diff < lowestDiff) {
+          lowestDiff = diff
+          lowestId = x
+        }
+      }
+      ratios.push(fromId(lowestId)[0] / fromId(lowestId)[1])
+    }
+
+    return ratios
+  }, [getEqualTemperamentRatios, highestDenominator, powersOf2Only])
+
   React.useEffect(() => {
     switch (scaleType) {
       case ScaleType.EQUAL: {
-        const k = Math.pow(octaveMult, 1/notesPerOctave)
-        const ratios = [k]
-        let ratio = k
-        for (let i=1; i<notesPerOctave-1; i++) {
-          ratio *= k
-          ratios.push(ratio)
-        }
-        setNotesViaRatios(ratios)
+        setNotesViaRatios(getEqualTemperamentRatios())
         break
       }
       case ScaleType.JUST: {
+        setNotesViaRatios(getJustIntonationRatios())
         break
       }
       case ScaleType.PYTHAGOREAN: {
@@ -92,13 +162,21 @@ function App() {
         break
       }
     }
-  },[referenceNote, octavesBelow, octavesAbove, scaleType, notesPerOctave, baseNum, baseDen, setNotesViaRatios, octaveMult])
+  },[referenceNote, octavesBelow, octavesAbove, scaleType, notesPerOctave, baseNum, baseDen, setNotesViaRatios, octaveMult, getEqualTemperamentRatios, highestDenominator, powersOf2Only, getJustIntonationRatios])
 
   const stringElements = React.useMemo(() => {
     return notes.map((n, i) => {
-      return (<String layoutId={`string${i}`} freq={n} referenceNote={referenceNote} />)
+      return (<String
+        layoutId={`string${i}`}
+        freq={n}
+        referenceNote={referenceNote}
+        logView={logView}
+        hoveredFreq={hoveredFreq}
+        onHoverStart={() => setHoveredFreq(n)}
+        onHoverEnd={() => setHoveredFreq(undefined)}
+      />)
     })
-  }, [notes, referenceNote])
+  }, [hoveredFreq, logView, notes, referenceNote])
 
   const makeHandleChangeInt = React.useCallback((setFunc: (n: number) => void, min: number, max: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
     const n = parseInt(e.target.value, 10)
@@ -108,16 +186,23 @@ function App() {
 
   return (
     <>
+      <motion.input type="checkbox" checked={logView} onChange={() => setLogView(!logView)}/>
       {scaleTypeSelect}
       <motion.input type="number" defaultValue={notesPerOctave} onChange={makeHandleChangeInt(setNotesPerOctave, 1, 20)}/>
       <motion.input type="number" defaultValue={referenceNote} onChange={makeHandleChangeInt(setReferenceNote, 100, 1000)}/>
+      {
+        scaleType === ScaleType.JUST && (<>
+          <motion.input type="checkbox" checked={powersOf2Only} onChange={() => setPowersOf2Only(!powersOf2Only)}/>
+          <motion.input type="number" defaultValue={highestDenominator} onChange={makeHandleChangeInt(setHighestDenominator, 8, 32)}/>
+        </>)
+      }
       {
         scaleType === ScaleType.PYTHAGOREAN && (<>
           <motion.input type="number" defaultValue={baseNum} onChange={makeHandleChangeInt(setBaseNum, 1, 20)}/>
           <motion.input type="number" defaultValue={baseDen} onChange={makeHandleChangeInt(setBaseDen, 100, 1000)}/>
         </>)
       }
-      <div className="mm-string-container">
+      <div className="mm-strings-container">
         {stringElements}
       </div>
     </>
--- src/components/String.tsx
+++ src/components/String.tsx
@@ -1,24 +1,66 @@
 import React from 'react'
 import { motion } from "motion/react"
+import './String.css'
 
-export const String = (props: { layoutId: string, freq: number, referenceNote: number }) => {
-  const { layoutId, freq, referenceNote } = props
+export const String = (props: {
+  layoutId: string,
+  freq: number,
+  referenceNote: number,
+  logView: boolean,
+  hoveredFreq?: number,
+  onHoverStart?: ()=>void,
+  onHoverEnd?: ()=>void;
+}) => {
+  const { layoutId, freq, referenceNote, logView, hoveredFreq, onHoverStart, onHoverEnd } = props
+
+  const height = React.useMemo(() => logView ? Math.log(freq)*100 : freq, [freq, logView])
 
   const hoverScaleY = React.useMemo(() => {
-    return 15 / freq + 1
-  }, [freq])
+    return 15 / height + 1
+  }, [height])
 
-  return (<motion.div
-    layoutId={layoutId}
-    style={{
-      width: 10,
-      height: freq,
-      backgroundColor: referenceNote === freq ? "#ff8800" : "#ff0088",
-      borderRadius: 5,
-    }}
-    initial={{ scale: 0 }}
-    animate={{ scale: 1 }}
-    whileHover={{ scaleY: hoverScaleY, scaleX: 1.2}}
-    transition={{ duration: 0.1 }}
-  />)
+  const backgroundColor = React.useMemo(() => {
+    if (referenceNote === freq) return "#3700b3"
+    if (Number.isInteger(referenceNote/freq) || Number.isInteger(freq/referenceNote)) return "#6200EE"
+    return "#03dac6"
+  }, [freq, referenceNote])
+
+  const isSemiHover = React.useMemo(() => {
+    if (!hoveredFreq) return false
+    if (Number.isInteger(hoveredFreq/freq) || Number.isInteger(freq/hoveredFreq)) return true
+  }, [freq, hoveredFreq])
+
+  const className = React.useMemo(() => {
+    let s = 'mm-string-container'
+    if (isSemiHover) s += ' semi-hover'
+    return s
+  }, [isSemiHover])
+
+  const animate = React.useMemo(() => {
+    if (isSemiHover) return { scaleY: hoverScaleY, scaleX: 1.2 }
+    return {}
+  },[hoverScaleY, isSemiHover])
+
+  return (<div className={className}>
+    <motion.div className='mm-string-label' animate={{ rotate: -90 }}>{freq}</motion.div>
+    <motion.div
+      layoutId={layoutId}
+      style={{
+        width: 8,
+        backgroundColor: backgroundColor,
+        borderRadius: 5,
+      }}
+      animate={{
+        ...animate,
+        height: height,
+        transition: {
+          height: { duration: 0.3, type: "ease" },
+          scaleY: { duration: 0.04, type: "ease" },
+          scaleX: { duration: 0.04, type: "ease" }
+        }
+      }}
+      onHoverStart={onHoverStart}
+      onHoverEnd={onHoverEnd}
+    />
+  </div>)
 }
